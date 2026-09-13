464d928a25b0fe35e4cd02641b3e279ff72a8569
ChurchillG <churchillg95@gmail.com>
2026-09-13 18:00:48 +0200

    added profilepage class

---
+++ src/pages/ProfilePage.ts
@@ -0,0 +1,25 @@
+import { Page } from '@playwright/test';
+import { BasePage } from '../helpers/BasePage';
+
+export class ProfilePage extends BasePage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  private get editProfileButton() {
+    return this.page.getByRole('button', { name: /edit profile/i });
+  }
+
+  private get profilePicture() {
+    return this.page.locator('[data-testid="profile-picture"], img.profile-picture');
+  }
+
+  async clickEditProfile(): Promise<void> {
+    await this.click(this.editProfileButton);
+  }
+
+  async getProfilePictureSrc(): Promise<string | null> {
+    await this.waitForVisible(this.profilePicture);
+    return this.profilePicture.getAttribute('src');
+  }
+}
